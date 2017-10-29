7408d4ecd40054d19e5b1ca8ad252fb8f0170324
ameikun <13618358620@163.com>
2017-10-29 11:38:23 +0800

    container the max water

--- src/solution/Containerwater.java
+++ src/solution/Containerwater.java
@@ -3,17 +3,30 @@ package solution;
 public class Containerwater {
 	
 	public int maxArea(int[] height) {
+		/*
 		int Area = 0,h;
 		int len = height.length;
 		for(int i = 0;i < (len-1);i ++)
 		{
-			if (height[i]<height[0]) {
+			if (i>0&&height[i]<height[i-1]) {
 				continue;
 			}
 			for(int j = (i+1);j < len;j ++){
 				h = height[i]<height[j] ? height[i]:height[j];
 				Area = ((j-i)*h)>Area ? ((j-i)*h):Area;
 			}
+		}*/
+		int Area = 0,h;
+		int len = height.length;
+		int i = 0,j = len - 1;
+		while (i<j) {
+			h = height[i]<height[j] ? height[i]:height[j];
+			Area = ((j-i)*h)>Area ? ((j-i)*h):Area;
+			if (height[i]<height[j]) {
+				i++;
+			} else {
+				j--;
+			}	
 		}
        return Area; 
     }
